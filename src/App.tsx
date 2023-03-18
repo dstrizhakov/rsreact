ccc0e7cb4e75743a4b916237d1956098ec746048
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-03-18 18:42:56 +0300

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import "./App.scss";
+import './App.scss';
 
 function App() {
   return (
     <div>
       <h1>Hello world</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-        nobis perspiciatis amet libero eius. Natus facere quos laborum maiores
-        eaque ducimus porro, blanditiis ex excepturi animi repellat deserunt
-        quasi consequatur?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nobis perspiciatis amet
+        libero eius. Natus facere quos laborum maiores eaque ducimus porro, blanditiis ex excepturi
+        animi repellat deserunt quasi consequatur?
       </p>
     </div>
   );
