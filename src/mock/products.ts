5a3e31f9de4d2a5c8e8018451ff1c4c3f2ae6069
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-03-20 17:00:19 +0300

    fix: mock data to separate file

---
+++ src/mock/products.ts
@@ -0,0 +1,27 @@
+const products = [
+  {
+    id: 1,
+    image: 'product01.jpg',
+    title: 'Arizona valley',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    price: 120,
+    likes: 27,
+  },
+  {
+    id: 2,
+    image: 'product02.jpg',
+    title: 'Arizona valley',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    price: 160,
+    likes: 0,
+  },
+  {
+    id: 3,
+    image: 'product03.jpg',
+    title: 'Arizona valley',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    price: 100,
+    likes: 10,
+  },
+];
+export default products;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,7 @@
 import Search from '../components/Search';
 import { Component, ReactNode } from 'react';
 import ProductList from '../components/ProductList';
-
-const products = [
-  {
-    id: 1,
-    image: 'product01.jpg',
-    title: 'Arizona valley',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    price: 120,
-    likes: 27,
-  },
-  {
-    id: 2,
-    image: 'product02.jpg',
-    title: 'Arizona valley',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    price: 160,
-    likes: 0,
-  },
-  {
-    id: 3,
-    image: 'product03.jpg',
-    title: 'Arizona valley',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    price: 100,
-    likes: 10,
-  },
-];
+import products from '../mock/products';
 
 class Home extends Component {
   render(): ReactNode {
