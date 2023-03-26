b220ae4b5277406a28ae847b21a1e0c9419c1b0c
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-03-26 12:34:04 +0300

    feat: add MyInput

--- src/types/Types.tsx
+++ src/types/Types.tsx
@@ -1,8 +1,14 @@
 export interface IProduct {
-  id: number;
+  id: number | null;
   image: string;
   title: string;
   text: string;
   price: number;
   likes?: number;
+}
+
+export interface IProductForm extends IProduct {
+  created: number | string;
+  isAvailable: boolean;
+  isSale: boolean;
 }
