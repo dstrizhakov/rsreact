308087a402a2c5b9044cced8950eaaeee9b15fe3
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-03-27 21:33:07 +0300

    fix: remove temp files

--- 123/src/types/home.ts
+++
@@ -1,9 +0,0 @@
-export type Book = {
-  author: string;
-  country: string;
-  imageLink: string;
-  language: string;
-  pages: number;
-  title: string;
-  year: number;
-};
--- src/types/Types.tsx
+++ src/types/Types.tsx
@@ -9,5 +9,3 @@ export interface IProduct {
   isAvailable?: boolean;
   isSale?: boolean;
 }
-
-
