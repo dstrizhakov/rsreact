26f1e4360926f9c3a1c7065bc2ad752941126c0f
dstrizhakov <dstrizhakov@gmail.com>
2023-04-26 12:00:15 +0300

    feat: add base ssr

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['/src/entry-client.tsx'],
+    },
+  },
   plugins: [react()],
   test: {
     globals: true,
